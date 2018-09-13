947869d75b0f8151dd051ce446af729498124232
Emba Moussa <emba.moussa@eventinc.de>
2018-09-13 12:46:13 +0200

    feat: add route and actions for diaries controller

---
+++ app/controllers/api/v1/diaries_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::DiariesController < ApplicationController
+  def index
+    @diaries = Diary.order_by(:fecha => 'desc')
+    render json: @diaries
+  end
+
+  def search
+    @search = Item.search params[:search]
+    render json: @search
+  end
+end
