68b5de8d990161eda238b8d08c6dab23ac708196
Emba Moussa <emba.moussa@eventinc.de>
2018-09-15 22:36:13 +0200

    feat: Items component, first component, might change it in the future to became Diary component and within this, create the nested component/atoms/etc

--- client/src/App.js
+++ client/src/App.js
@@ -5,14 +5,12 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className="BoeApp">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+          <h1 className="App-title">B.O.E</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <ItemsContainer />
       </div>
     );
   }
---
+++ client/src/components/ItemsContainer.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class ItemsContainer extends Component {
+  render() {
+    return (
+      <div className="Items-container">
+        Lists
+            </div>
+    )
+  }
+}
+
+export default ItemsContainer;
