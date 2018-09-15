67af44067cb9a797267e85cb2eb61ba3dcc092fc
Emba Moussa <emba.moussa@eventinc.de>
2018-09-16 01:45:50 +0200

    feat: change logo asset and use styling class to default name

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import logo from './boe_logo.svg';
 import './App.css';
 import DiariesContainer from './components/DiariesContainer';
 
@@ -8,8 +8,7 @@ class App extends Component {
     return (
       <div className="BoeApp">
         <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">B.O.E</h1>
+          <img src={logo} className="BoeApp-logo" alt="logo" />
         </header>
         <DiariesContainer />
       </div>
