c674bc3f939e72cbc6fe09e9267d91905c64115c
Emba Moussa <emba.moussa@eventinc.de>
2018-09-16 19:38:37 +0200

    feat: add Item source index action route

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     namespace :v1 do
       resources :diaries, only: [:index] do
         resources :sections, only: [:index] do
-          resources :departments, only: [:index]
+          resources :departments, only: [:index] do
+            resources :items, only: [:index]
+          end
         end
         collection do
           get 'search'
