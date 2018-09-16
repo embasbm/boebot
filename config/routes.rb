e8e72f4e823b05267d333f359086419dbc8b7bff
Emba Moussa <emba.moussa@eventinc.de>
2018-09-16 12:59:41 +0200

    feat: routes for nested resources

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :diaries, only: [:index] do
+        resources :sections, only: [:index] do
+          resources :departments, only: [:index]
+        end
         collection do
           get 'search'
         end
