aa0b589b4b8335e421817cdbc4867f97c1e37b0b
Emba Moussa <emba.moussa@eventinc.de>
2018-09-16 13:01:56 +0200

    feat: make Diary component show all nested section

--- client/src/components/Diary.js
+++ client/src/components/Diary.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { Component } from 'react';
+import axios from 'axios';
+import SectionsContainer from "./SectionsContainer";
+class Diary extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      sections: []
+    }
+  }
 
-const Diary = ({ diary, index }) =>
-  <div className="single-item" key={index}>
-    <h4>{diary.date_pub}</h4>
-    <p>{diary.pdf_url}</p>
-  </div>
+  componentDidMount() {
+    axios.get('api/v1/diaries/' + this.props.diary._id.$oid + '/sections')
+      .then(response => {
+        console.log(response);
+        this.setState({
+          sections: response.data
+        })
+      })
+      .catch(error => console.log(error))
+  }
 
+  render() {
+    return (
+      <div className="diary-single-item">
+        <h4>{this.props.diary.date_pub}</h4>
+        <p>{this.props.diary.pdf_url}</p>
+        <div className="sections">
+          <SectionsContainer sections={this.state.sections} diary={this.props.diary} />
+        </div>
+      </div >
+    )
+  }
+}
 export default Diary;
