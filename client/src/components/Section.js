81f421d233441f718a49a8ef176dcdd3e23e615a
Emba Moussa <emba.moussa@eventinc.de>
2018-09-16 21:46:15 +0200

    feat: Section component

---
+++ client/src/components/Section.js
@@ -0,0 +1,42 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import DepartmentsContainer from "./DepartmentsContainer";
+
+class Section extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      departments: []
+    }
+  }
+
+  componentDidMount() {
+    axios.get('api/v1/diaries/' + this.props.diary._id.$oid + '/sections/' + this.props.section._id.$oid + '/departments')
+      .then(response => {
+        this.setState({
+          departments: response.data
+        })
+      })
+      .catch(error => console.log(error))
+  }
+
+  render() {
+    return (
+      <div className="section-single-item">
+        <div className="departments-list">
+          {this.state.departments.map((department, index) => {
+            return (
+              <DepartmentsContainer
+                diary={this.props.diary}
+                section={this.props.section}
+                departments={this.state.departments}
+              />
+            )
+          })}
+        </div>
+      </div >
+    )
+  }
+}
+
+export default Section;
