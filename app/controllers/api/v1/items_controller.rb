b9f92b1757379a1df04041f7e6d84f10b52823dd
Emba Moussa <emba.moussa@eventinc.de>
2018-09-16 23:42:14 +0200

    feat: fetch items depending on epigraph_id presence, it there is epigraph_id, get only that epigraph item, otherwise, bring all department epigraphs items

--- app/controllers/api/v1/items_controller.rb
+++ app/controllers/api/v1/items_controller.rb
@@ -1,6 +1,15 @@
 class Api::V1::ItemsController < ApplicationController
   def index
-    @items = Department.find(params[:department_id])&.items
-    render json: @items
+
+    if params[:epigraph_id].present?
+      @epigraph = Epigraph.find(params[:epigraph_id])
+      @items = @epigraph&.items
+      @epigraphs = [@epigraph]
+    else
+      @department = Department.find(params[:department_id])
+      @items = @department&.items
+      @epigraphs = @department&.epigraphs
+    end
+    render json: {items: @items, epigraphs: @epigraphs}
   end
 end
