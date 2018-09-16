39e648ac3609888865f0c2eb1374d2ab4a27cbdc
Emba Moussa <emba.moussa@eventinc.de>
2018-09-16 23:40:06 +0200

    feat: EpigraphContainer component

---
+++ client/src/components/EpigraphsContainer.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react';
+import Epigraph from "./Epigraph";
+
+class EpigraphsContainer extends Component {
+  render() {
+    return (
+      <div className="EpigraphsContainer">
+        <div className="epigraphs-list">
+          {this.props.epigraphs.map((epigraph, index) => {
+            return (
+              <Epigraph
+                diary={this.props.diary}
+                section={this.props.section}
+                department={this.props.department}
+                epigraph={epigraph}
+                index={index}
+              />
+            )
+          })}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default EpigraphsContainer;
