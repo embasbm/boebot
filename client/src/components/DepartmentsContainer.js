457b75ae239d0706f10b521962f1f4471a0e8c74
Emba Moussa <emba.moussa@eventinc.de>
2018-09-16 21:22:07 +0200

    feat: DeparmentsContainer component

---
+++ client/src/components/DepartmentsContainer.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Department from "./Department";
+
+class DepartmentsContainer extends Component {
+  render() {
+    return (
+      <div className="Departments-container">
+        {this.props.departments.map((department, index) => {
+          return (<Department diary={this.props.diary} section={this.props.section} department={department} key={index} />)
+        })}
+      </div>
+    )
+  }
+}
+
+export default DepartmentsContainer;
