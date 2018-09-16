3f7e9c12e3f7e77b0fb69d143393556bd201e4e8
Emba Moussa <emba.moussa@eventinc.de>
2018-09-16 21:20:45 +0200

    feat: remove unused constructor in SectionContainer component

--- client/src/components/SectionsContainer.js
+++ client/src/components/SectionsContainer.js
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 import Section from "./Section";
 
 class SectionsContainer extends Component {
-  constructor(props) {
-    super(props)
-  }
+  // constructor(props) {
+  //   super(props)
+  // }
 
   render() {
     return (
