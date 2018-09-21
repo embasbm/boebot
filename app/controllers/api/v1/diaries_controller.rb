4c911fb903004863bf099bffa05fa2727c495f50
Emba Moussa <emba.moussa@eventinc.de>
2018-09-21 22:03:57 +0200

    feat: fetch data in case there are no diaries data

--- app/controllers/api/v1/diaries_controller.rb
+++ app/controllers/api/v1/diaries_controller.rb
@@ -1,4 +1,6 @@
 class Api::V1::DiariesController < ApplicationController
+  before_action :seed_data, only: [:index]
+
   def index
     @diaries = Diary.order_by(:fecha => 'desc')
     render json: @diaries
@@ -7,5 +9,19 @@ class Api::V1::DiariesController < ApplicationController
   def search
     @search = Item.search params[:search]
     render json: {search: @search}
+  end
+
+  private
+  def seed_data
+    t=Time.now
+    return if t.sunday? || t.saturday?
+    if Diary.last.fecha.to_date != Date.today || Diary.all.empty?
+      ImportData.new
+      reindex_all_items
+    end
+  end
+
+  def reindex_all_items
+    %x[rake searchkick:reindex:all]
   end
 end
